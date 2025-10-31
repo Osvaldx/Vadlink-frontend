44da06fd57e7fe619b281d6af4377be2e186968c
Osvaldx <romanonahuel24@gmail.com>
2025-10-31 17:28:09 -0300

    [+] se crearon las rutas de la pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        children: [
+            { path: 'login', loadComponent: () => import('./pages/auth/login/login').then(m => m.Login) },
+            { path: 'register', loadComponent: () => import('./pages/auth/register/register').then(m => m.Register) }
+        ]
+    },
+    {
+        path: '',
+        children: [
+            { path: 'posts', loadComponent: () => import('./pages/posts/posts').then(m => m.Posts) },
+            { path: 'profile', loadComponent: () => import('./pages/profile/profile').then(m => m.Profile) }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    }
+];
