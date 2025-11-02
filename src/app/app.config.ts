92693c9a661d7e632bd3ec7afe06547e797559e6
Osvaldx <romanonahuel24@gmail.com>
2025-11-02 01:08:03 -0300

    [+] se importan los iconos globalmente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideIcons } from '@ng-icons/core';
+import { heroUserSolid, heroEyeSolid, heroEyeSlashSolid, heroKeySolid } from '@ng-icons/heroicons/solid';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideIcons({ heroUserSolid, heroEyeSolid, heroEyeSlashSolid, heroKeySolid })
   ]
 };
