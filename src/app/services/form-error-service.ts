bb03e703a373a4a2edbf76e89ecd56e08daee148
Osvaldx <romanonahuel24@gmail.com>
2025-11-02 23:00:22 -0300

    [+] se agregaron más errores de formulario

--- src/app/services/form-error-service.ts
+++ src/app/services/form-error-service.ts
@@ -17,16 +17,44 @@ export class FormErrorService {
 
     if(errors['required']) {
       return '[!] Este campo es requerido!';
-    } else if(errors['email']) {
+    }
+
+    else if(errors['email']) {
       const details = errors['email'];
       if(!details.hasCorrectMail) return '[!] El correo es invalido!';
-    } else if(errors['passwordWrong']) {
+    }
+
+    else if(errors['minlength']) {
+      const details = errors['minlength'];
+      return `[!] El minimó de caracteres es: ${details.requiredLength}`;
+    }
+
+    else if(errors['maxlength']) {
+      const details = errors['maxlength'];
+      return `[!] El maximo de caracteres es: ${details.requiredLength}`;
+    }
+
+    else if(errors['passwordWrong']) {
       const details = errors['passwordWrong'];
       if(!details.hasMinLength) return '[!] La contraseña tiene que ser mayor a 8 caracteres!';
       if(!details.hasButOneCapitalLetter) return '[!] La contraseña tiene que tener al menos 1 letra en mayus!';
       if(!details.hasButOneNumber) return '[!] La contraseña tiene que tener al menos 1 número!';
     }
 
+    else if(errors['validateDateOfBirth']) {
+      const details = errors['validateDateOfBirth'];
+      if(details.isFuture) return '[!] La fecha de nacimiento no puede ser futura';
+      if(details.isOld) return '[!] La fecha indica que eres muy mayor';
+      if(details.isYoung) return '[!] Eres demasiado joven';
+    }
+    
+    else if(errors['InvalidImage']) {
+      const details = errors['InvalidImage'];
+      if(details.tooBig) return `[!] Archivo demasiado grande MAXIMO: ${details.requiredSize}`;
+      if(details.badType) return `[!] Archivos permitidos: ${details.availableTypes}`;
+    }
+
+    console.warn(errors);
     return '[!] Error no registrado:';
   }
   
