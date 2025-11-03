055829fa466a7137fccb6fc6f5b0c848d5a1c71b
Osvaldx <romanonahuel24@gmail.com>
2025-11-02 22:58:28 -0300

    [+] se trajo todos los iconos que vamos a usa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,37 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideIcons } from '@ng-icons/core';
-import { heroUserSolid, heroEyeSolid, heroEyeSlashSolid, heroKeySolid } from '@ng-icons/heroicons/solid';
+import {
+  heroUserSolid,
+  heroEyeSolid,
+  heroEyeSlashSolid,
+  heroKeySolid,
+  heroTagSolid,
+  heroIdentificationSolid,
+  heroCalendarDateRangeSolid,
+  heroUsersSolid,
+  heroChatBubbleBottomCenterTextSolid,
+  heroCloudArrowUpSolid,
+  heroTrashSolid
+ } from '@ng-icons/heroicons/solid';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideIcons({ heroUserSolid, heroEyeSolid, heroEyeSlashSolid, heroKeySolid })
+    provideIcons({
+      heroUserSolid,
+      heroEyeSolid,
+      heroEyeSlashSolid,
+      heroKeySolid,
+      heroTagSolid,
+      heroIdentificationSolid,
+      heroCalendarDateRangeSolid,
+      heroUsersSolid,
+      heroChatBubbleBottomCenterTextSolid,
+      heroCloudArrowUpSolid,
+      heroTrashSolid
+    })
   ]
 };
