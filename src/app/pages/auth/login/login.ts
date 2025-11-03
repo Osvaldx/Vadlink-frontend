3683e47e3682a3941cd03836dcfe178840b97040
Osvaldx <romanonahuel24@gmail.com>
2025-11-02 23:00:00 -0300

    [+] se agrego validate email

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,39 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgIcon } from '@ng-icons/core';
 import { RouterLink } from "@angular/router";
 import { ButtonClickAnimation } from "../../../directives/button-click-animation";
 import { ValidateErrorsInput } from "../../../directives/validate-errors-input";
-
-export function ValidatepasswordWrong(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const cadena = control.value;
-    const regExButOneCapitalLetter = new RegExp('^(?=.*[A-Z]).+$');
-    const regExButOneNumber = new RegExp('[0-9]');
-
-    const hasMinLengthTest = ((String (cadena)).length >= 8);
-    const hasButOneCapitalLetterTest = regExButOneCapitalLetter.test(control.value);
-    const hasButOneNumberTest = regExButOneNumber.test(control.value);
-    
-    return (hasMinLengthTest && hasButOneCapitalLetterTest && hasButOneNumberTest) ? null : {
-      passwordWrong: {
-        hasMinLength: hasMinLengthTest,
-        hasButOneCapitalLetter: hasButOneCapitalLetterTest,
-        hasButOneNumber: hasButOneNumberTest
-      }
-    }
-  }
-}
-
-export function ValidationEmail(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const regExMail = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-
-    const hasCorrectMail = regExMail.test(control.value);
-
-    return (hasCorrectMail) ? null : { email: { hasCorrectMail: hasCorrectMail } };
-  }
-}
+import { ValidatepasswordWrong } from '../../../validators/passwordWrong.validator';
+import { ValidationEmail } from '../../../validators/email.validator';
 
 @Component({
   selector: 'app-login',
@@ -43,7 +15,6 @@ export function ValidationEmail(): ValidatorFn {
 })
 export class Login {
 
-
   public loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, ValidationEmail()]),
     password: new FormControl('', [Validators.required, ValidatepasswordWrong()])
@@ -51,7 +22,7 @@ export class Login {
   
   public showPassword = signal<boolean>(false);
 
-  public togglePassord(): void {
+  public togglePassword(): void {
     this.showPassword.update(p => !p);
   }
 
