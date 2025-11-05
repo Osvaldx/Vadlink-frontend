8ed548609c6c65b14aacb0b132a8f2f4a15473c9
Osvaldx <romanonahuel24@gmail.com>
2025-11-05 00:11:07 -0300

    [+] boton de cerrar sesion temporal

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
 export class MainLayout {
+
+  constructor(private readonly authService: Auth, private readonly router: Router) { }
+
+  async logOut(){
+    this.authService.signOut();
+  }
 
 }
