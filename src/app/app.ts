643708affc4d15115a7e297a2ba5c1b33341de92
Osvaldx <romanonahuel24@gmail.com>
2025-11-05 17:35:34 -0300

    [+] se importo el toast

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastMessage } from './components/toast-message/toast-message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastMessage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
