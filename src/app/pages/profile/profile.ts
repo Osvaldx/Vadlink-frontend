20568e66a674d0aaae5925f886c99d03ca42ca50
Osvaldx <romanonahuel24@gmail.com>
2025-11-05 01:40:18 -0300

    [+] datos temporales para mostrar

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth, UserData } from '../../services/auth';
 
 @Component({
   selector: 'app-profile',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './profile.css',
 })
 export class Profile {
+
+  public data!: UserData | null;
+
+  constructor(private readonly authService: Auth) {
+    if(authService.currentUser()) {
+      this.data = authService.currentUser();
+    }
+  }
 
 }
