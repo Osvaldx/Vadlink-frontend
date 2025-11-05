fcbd2304ba830f005707ebd9b7a8a8d2861e60b4
Osvaldx <romanonahuel24@gmail.com>
2025-11-05 17:35:23 -0300

    [+] componente para los toast

---
+++ src/app/components/toast-message/toast-message.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-toast-message',
+  imports: [NgIcon],
+  templateUrl: './toast-message.html',
+  styleUrl: './toast-message.css',
+})
+export class ToastMessage {
+
+}
