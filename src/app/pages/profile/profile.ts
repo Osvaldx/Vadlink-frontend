9b7c4bd2c0716bc54c83f40833c4c01289dc0e20
Osvaldx <romanonahuel24@gmail.com>
2025-11-07 01:38:18 -0300

    [+] se parseo las fechas a Date

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { UserData } from '../../interfaces/user-data';
+import { NgIcon } from '@ng-icons/core';
+import { DatePipe } from '@angular/common';
+import { CustomPost } from "../../components/custom-post/custom-post";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [NgIcon, DatePipe, CustomPost],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
 
   public data!: UserData | null;
+  public create_at!: Date;
+  public dateofbirth!: Date;
 
   constructor(private readonly authService: Auth) {
     if(authService.currentUser()) {
       this.data = authService.currentUser();
+      this.create_at = new Date(this.data?.createDate!);
+      this.dateofbirth = new Date(this.data?.dateofbirth!);
     }
   }
 
