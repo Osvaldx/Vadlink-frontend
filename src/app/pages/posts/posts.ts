978352b59fe25fb28daeed1aa2439b377a13e773
Osvaldx <romanonahuel24@gmail.com>
2025-11-11 02:05:36 -0300

    [+] agregamos toda la logica de la pagina post

--- src/app/pages/posts/posts.ts
+++ src/app/pages/posts/posts.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { PostFormat } from '../../interfaces/post-format';
+import { PostsService } from '../../services/posts-service';
+import { AsyncPipe } from '@angular/common';
+import { CustomPost } from "../../components/custom-post/custom-post";
+import { UserData } from '../../interfaces/user-data';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [AsyncPipe, CustomPost],
   templateUrl: './posts.html',
   styleUrl: './posts.css',
 })
-export class Posts {
+export class Posts implements OnInit{
+
+  public posts$!: Observable<PostFormat[]>
+  public user!: UserData;
+
+  constructor(private readonly postsService: PostsService, private readonly authService: Auth) { }
+
+  ngOnInit(): void {
+    const userOrNull = this.authService.currentUser();
+    if(userOrNull) {
+      this.user = userOrNull;
+    }
+
+    this.postsService.getPostsLocal({ date: 'desc', limit: 10 });
+    this.posts$ = this.postsService.getPostsObservable();
+  }
 
 }
