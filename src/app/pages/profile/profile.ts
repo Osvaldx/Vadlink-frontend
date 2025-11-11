9768e63a4dcf544553e8ec63851d87a16f1ee528
Osvaldx <romanonahuel24@gmail.com>
2025-11-10 22:26:21 -0300

    [+] ahora el perfil es completamente funcional mostrando reales publicaciones

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,28 +1,52 @@
-import { Component, Pipe } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { UserData } from '../../interfaces/user-data';
 import { NgIcon } from '@ng-icons/core';
-import { DatePipe } from '@angular/common';
 import { CustomPost } from "../../components/custom-post/custom-post";
+import { InfoProfile } from '../../components/info-profile/info-profile';
+import { PostsService } from '../../services/posts-service';
+import { PostFormat } from '../../interfaces/post-format';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [NgIcon, DatePipe, CustomPost],
+  imports: [NgIcon, CustomPost, InfoProfile, AsyncPipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
+export class Profile implements OnInit{
 
   public data!: UserData | null;
   public create_at!: Date;
   public dateofbirth!: Date;
 
-  constructor(private readonly authService: Auth) {
-    if(authService.currentUser()) {
-      this.data = authService.currentUser();
+  private postsSubject = new BehaviorSubject<PostFormat[]>([]);
+  public posts$ = this.postsSubject.asObservable();
+
+  constructor(private readonly authService: Auth, private readonly postsService: PostsService) { }
+
+  ngOnInit(): void {
+    if(this.authService.currentUser()) {
+      this.data = this.authService.currentUser();
       this.create_at = new Date(this.data?.createDate!);
       this.dateofbirth = new Date(this.data?.dateofbirth!);
     }
+    
+    this.getPosts();
+  }
+
+  public getPosts() {
+    if(this.data) {
+      this.postsService.findAllPostsWithUsername(this.data.username).subscribe(posts => {
+        this.postsSubject.next(posts);
+      })
+    }
+  }
+
+  public postDeleted(id: string) {
+    const updated = this.postsSubject.value.filter(p => p._id != id);
+    this.postsSubject.next(updated); 
   }
 
 }
