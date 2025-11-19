cf766a49c37e33e5fe6fddbbe10035d567431d0d
Osvaldx <romanonahuel24@gmail.com>
2025-11-19 19:27:03 -0300

    [+] se agregaron y se consumieron las rutas de los nuevos refresh token y authorize y se creo toda la logica

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { UserData } from '../interfaces/user-data';
 import { SignInCredentials } from '../interfaces/sign-in-credentials';
 import { MessageManager } from './message-manager';
+import { RefreshData } from '../interfaces/refresh-data';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +14,14 @@ export class Auth {
 
   private apiUrl = 'https://vadlink-backend.vercel.app';
   // private apiUrl = 'http://localhost:3000';
+  private refreshTimer: NodeJS.Timeout | null = null;
+  private logoutTimer: NodeJS.Timeout | null = null;
+  
+  private ModalSubject = new BehaviorSubject<boolean>(false);
+  public ModalObservable$ = this.ModalSubject.asObservable();
+
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public $loading = this.loadingSubject.asObservable();
 
   public currentUser = signal<UserData | null>(null);
 
@@ -19,14 +29,15 @@ export class Auth {
     private readonly httpClient: HttpClient,
     private readonly router: Router,
     private readonly msgManager: MessageManager
-  ) { }
+  ) { this.resetModalSubject() }
 
   public signIn(credentials: SignInCredentials) {
-    this.httpClient.post(this.apiUrl + '/auth/login', credentials).subscribe({
+    this.httpClient.post<UserData>(this.apiUrl + '/auth/login', credentials).subscribe({
       next: (user) => {
         this.msgManager.add('success', 'Inicio de sesión exitoso!', 3);
         this.currentUser.set(user as UserData);
         console.log(this.currentUser());
+        this.startTokenTimer(user.exp);
         this.router.navigateByUrl('/');
       },
       error: (err: HttpErrorResponse) => {
@@ -39,11 +50,12 @@ export class Auth {
   }
 
   public signUp(formData: FormData) {
-    this.httpClient.post(this.apiUrl + '/auth/register', formData).subscribe({
+    this.httpClient.post<UserData>(this.apiUrl + '/auth/register', formData).subscribe({
       next: (user) => {
         this.msgManager.add('success', 'Registro exitoso!', 3);
         this.currentUser.set(user as UserData);
         console.log(this.currentUser());
+        this.startTokenTimer(user.exp);
         this.router.navigate(['/']);
       },
       error: (err: HttpErrorResponse) => {
@@ -72,26 +84,80 @@ export class Auth {
   }
 
   public loadCurrentUser() {
+    this.loadingSubject.next(true);
     return new Promise<void>((resolve) => {
-      this.httpClient.post(this.apiUrl + '/auth/data', {}).subscribe({
+      this.httpClient.post<UserData>(this.apiUrl + '/auth/authorize', {}).subscribe({
         next: (user) => {
+          console.log(user);
           this.currentUser.set(user as UserData);
+          this.startTokenTimer(user.exp);
+          this.router.navigate(['/posts']);
           resolve();
         },
         error: (err: HttpErrorResponse) => {
           console.log(err);
           this.currentUser.set(null);
+          this.router.navigate(['/auth/login']);
           resolve();
         }
       });
     })
   }
 
-  public verifyJwt() {
-    const request = this.httpClient.post(this.apiUrl + '/auth/data', {});
+  public refreshToken() {
+    this.httpClient.post<RefreshData>(this.apiUrl + '/auth/refresh', {}).subscribe({
+      next: (res) => {
+        console.log(res.message);
+        this.startTokenTimer(res.exp);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('Error al refrescar token:', err.error.message);
+        this.currentUser.set(null);
+        this.router.navigate(['/auth/login']);
+      }
+    });
+  }
 
-    request.subscribe((request) => {
-      console.log(request);
-    })
+  private startTokenTimer(exp: number) {
+    if (this.refreshTimer) clearTimeout(this.refreshTimer);
+    if(this.logoutTimer) clearTimeout(this.logoutTimer);
+
+    if (!this.currentUser()) return;
+
+    // UNIX timestamp en segundos
+    const expMs = exp * 1000;
+    const now = Date.now();
+
+    const refreshAt = expMs - (10 * 60 * 1000);
+
+    const refreshDelay = refreshAt - now;
+    const logoutDelay = expMs - now;
+
+    this.refreshTimer = setTimeout(() => {
+      this.ModalSubject.next(true);
+    }, refreshDelay);
+  
+    this.logoutTimer = setTimeout(() => {
+      this.currentUser.set(null);
+      this.resetModalSubject();
+      this.router.navigate(['/auth/login']);
+    }, logoutDelay);
+
+  }
+
+  public resetModalSubject() {
+    this.ModalSubject.next(false);
+  }
+
+  public getModalObservable() {
+    return this.ModalObservable$;
+  }
+
+  public getLoadingObservable() {
+    return this.$loading;
+  }
+
+  public setLoading(value: boolean) {
+    this.loadingSubject.next(value);
   }
 }
