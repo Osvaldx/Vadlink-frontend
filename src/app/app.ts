f92fb60259ce84479167bab0051922c3d4c926df
Osvaldx <romanonahuel24@gmail.com>
2025-11-19 19:27:44 -0300

    [+] se agrego la logica para poder manejar la pantalla de carga al iniciar la app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastsManager } from './components/toasts-manager/toasts-manager';
+import { RefreshModal } from "./components/refresh-modal/refresh-modal";
+import { ScreenLoading } from "./components/screen-loading/screen-loading";
+import { Auth } from './services/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastsManager],
+  imports: [RouterOutlet, ToastsManager, RefreshModal, ScreenLoading],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('vadlink-frontend');
+
+  constructor(private readonly authService: Auth) {}
+
+  ngOnInit(): void {
+    this.authService.loadCurrentUser().then(() => {
+      this.authService.setLoading(false);
+    })
+  }
 }
