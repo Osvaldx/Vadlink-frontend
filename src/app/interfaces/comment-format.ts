be5f50b1ec2e6f4c79d7dca58fde601a4977000c
Osvaldx <romanonahuel24@gmail.com>
2025-11-20 00:41:45 -0300

    [+] formato de como llegan los comentarios

---
+++ src/app/interfaces/comment-format.ts
@@ -0,0 +1,14 @@
+export interface CommentFormat {
+    _id: string;
+    post_id: string;
+    user_id: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    text: string;
+    modified: boolean;
+    avatar: string;
+    created_at: string;
+    __v: number;
+  }
+  
