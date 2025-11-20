4f83035d71c9cc2ebe54eacadf866205bb30e412
Osvaldx <romanonahuel24@gmail.com>
2025-11-20 00:40:09 -0300

    [+] se creo el servicio para consumir la api de los comments

---
+++ src/app/services/comments-service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CommentsParams } from '../interfaces/comments-params';
+import { FindCommentsFormat } from '../interfaces/find-comments-format';
+import { CommentFormat } from '../interfaces/comment-format';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentsService {
+
+  private apiUrl = 'http://localhost:3000/comments';
+
+  constructor(private http: HttpClient) {}
+
+  public findComments(postId: string, filters: CommentsParams) {
+    let query = this.apiUrl + `/${postId}?`;
+
+    if(filters.limit) {
+      query += `&limit=${filters.limit}`;
+    }
+    
+    if(filters.offset) {
+      query += `&offset=${filters.offset}`;
+    }
+
+    return this.http.get<FindCommentsFormat>(query);
+  }
+
+  public createComment(postId: string, text: string) {
+    return this.http.post<CommentFormat>(this.apiUrl + `/add/${postId}`, { text });
+  }
+
+  public updateComment(commentId: string, text: string) {
+    return this.http.put<CommentFormat>(this.apiUrl + `/update/${commentId}`, { text });
+  }
+  
+}
