d8ced45f6d175a8754a4ad8b10039c5b01cd6629
Osvaldx <romanonahuel24@gmail.com>
2025-11-23 19:51:36 -0300

    [+] se cambio el arbol de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { homeGuard } from './guards/home-guard';
+import { adminGuard } from './guards/admin-guard';
 
 export const routes: Routes = [
     {
@@ -12,11 +13,24 @@ export const routes: Routes = [
     },
     {
         path: '',
-        canActivate: [homeGuard],
         component: MainLayout,
         children: [
-            { path: 'posts', loadComponent: () => import('./pages/posts/posts').then(m => m.Posts) },
-            { path: 'profile', loadComponent: () => import('./pages/profile/profile').then(m => m.Profile) }
+            {
+                path: '',
+                canActivate: [homeGuard],
+                children: [
+                    { path: 'posts', loadComponent: () => import('./pages/posts/posts').then(m => m.Posts) },
+                    { path: 'profile', loadComponent: () => import('./pages/profile/profile').then(m => m.Profile) }
+                ]
+            },
+            {
+                path: 'dashboard',
+                canActivate: [adminGuard],
+                children: [
+                    { path: 'users', loadComponent: () => import('./pages/dashboard/users/users').then(m => m.Users) },
+                    { path: 'stats', loadComponent: () => import('./pages/dashboard/stats/stats').then(m => m.Stats) }
+                ]
+            },
         ]
     },
     {
