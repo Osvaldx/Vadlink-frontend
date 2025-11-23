f4b249823062693ac60f118ef5356d484b0f979e
Osvaldx <romanonahuel24@gmail.com>
2025-11-23 20:15:10 -0300

    [+] se agregaron opciones para administradores

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavButton } from "../nav-button/nav-button";
 import { Auth } from '../../services/auth';
+import { UserData } from '../../interfaces/user-data';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,9 +9,17 @@ import { Auth } from '../../services/auth';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
+export class NavBar implements OnInit {
+
+  public user!: UserData | null;
+
+  public styles = 'bg-neutral-800/90 shadow-xl/20 px-2 rounded-2xl flex justify-evenly items-center max-sm:w-full max-sm:justify-between max-sm:px-8 ';
 
   constructor(private readonly authService: Auth) { }
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser();
+  }
 
   async logOut(){
     this.authService.signOut();
